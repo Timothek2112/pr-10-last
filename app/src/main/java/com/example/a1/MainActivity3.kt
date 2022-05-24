c211630cf3c479c563dc4e50cdd2d0843bc25cfd
Дед <nikzav6080@gmail.com>
2022-05-24 23:07:33 +0700

    Initial commit

---
+++ app/src/main/java/com/example/a1/MainActivity3.kt
@@ -0,0 +1,22 @@
+package com.example.a1
+
+
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+
+
+
+class MainActivity3: AppCompatActivity () {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+
+}
+
+
